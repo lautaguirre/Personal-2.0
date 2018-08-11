97681ff8f83c3424db7eb33709fa6daebdde283e
Lautaro Aguirre <lauta.nahuel94@gmail.com>
2018-08-11 16:58:52 -0300

    Added images, favicon and base sass,

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.scss';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { Alert, Button } from 'react-bootstrap';
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUser } from '@fortawesome/free-solid-svg-icons'
-
-library.add(faUser);
+import { faEdit } from '@fortawesome/free-solid-svg-icons'
+library.add(faEdit);
 
 class App extends Component {
   render() {
     return (
       <div className='app' >
         <header className='appHeader' >
-          <img src={logo} className='appLogo' alt="logo" />
-          <h1 className='appTitle' >Welcome to React <FontAwesomeIcon icon={faUser} /></h1>
+          <h1 className='appHeaderTitle' >Welcome to React</h1>
         </header>
         <p className='appIntro' >
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit <code>src/App.js</code> and save to reload. <FontAwesomeIcon icon={faEdit} />
         </p>
         <Alert bsStyle="danger">
           <strong>Holy guacamole!</strong> Best check yo self, you're not looking too
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+import App from './components/App';
+
+import './index.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
