c8a7448714d867d4498a3140d31e3dae76c8c918
Lautaro Aguirre <lauta.nahuel94@gmail.com>
2018-08-11 12:45:28 -0300

    Added Sass

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className='app' >
+        <header className='appHeader' >
+          <img src={logo} className='appLogo' alt="logo" />
+          <h1 className='appTitle' >Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <p className='appIntro' >
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
