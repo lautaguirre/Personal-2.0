ef8131ce3e65f17dfb05ac95b2ee7599205af154
Lautaro Aguirre <lauta.nahuel94@gmail.com>
2018-08-12 01:59:25 -0300

    Finished NavBar

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,30 @@
 import React, { Component } from 'react';
-import './App.scss';
 
 import { Alert, Button } from 'react-bootstrap';
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEdit } from '@fortawesome/free-solid-svg-icons'
-library.add(faEdit);
+import NavigationBar from './NavigationBar/NavigationBar'
+
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className='app' >
-        <header className='appHeader' >
-          <h1 className='appHeaderTitle' >Welcome to React</h1>
-        </header>
-        <p className='appIntro' >
-          To get started, edit <code>src/App.js</code> and save to reload. <FontAwesomeIcon icon={faEdit} />
-        </p>
-        <Alert bsStyle="danger">
-          <strong>Holy guacamole!</strong> Best check yo self, you're not looking too
-          good.
-        </Alert>
-        <Button bsStyle="primary">Primary</Button>
+      <div>
+        <NavigationBar />
+
+        <div className='app'>
+          <header className='appHeader' >
+            <h1 className='appHeaderTitle' >Welcome to React</h1>
+          </header>
+          <p className='appIntro' >
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+          <Alert bsStyle="danger">
+            <strong>Holy guacamole!</strong> Best check yo self, you're not looking too
+            good.
+          </Alert>
+          <Button bsStyle="primary">Primary</Button>
+        </div>
       </div>
     );
   }
---
+++ src/components/NavigationBar/NavigationBar.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+import { Navbar, NavItem, Nav, Image } from 'react-bootstrap';
+
+import terminal from '../../assets/images/terminal.svg';
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome, faUser, faImages, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+library.add(faHome, faUser, faImages, faEnvelope);
+
+class NavigationBar extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar collapseOnSelect fixedTop >
+          <Navbar.Header>
+            <Navbar.Brand>
+               <Image src={terminal} />
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav>
+              <NavItem eventKey={1} href="#home">
+                Home &nbsp;<FontAwesomeIcon icon={faHome} />
+              </NavItem>
+              <NavItem eventKey={2} href="#about">
+                About &nbsp;<FontAwesomeIcon icon={faUser} />
+              </NavItem>
+              <NavItem eventKey={3} href="#portfolio">
+                Portfolio &nbsp;<FontAwesomeIcon icon={faImages} />
+              </NavItem>
+              <NavItem eventKey={4} href="#contact">
+                Contact &nbsp;<FontAwesomeIcon icon={faEnvelope} />
+              </NavItem>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default NavigationBar;
