f59eaac7c0d2c3ed76e5ea3abe6dab0d297023bd
Lautaro Aguirre <lauta.nahuel94@gmail.com>
2023-01-08 21:53:38 -0300

    update fonts ascent override

--- styles/fonts.ts
+++ styles/fonts.ts
@@ -2,19 +2,25 @@ import localFont from "@next/font/local";
 
 export const boldFont = localFont({
   src: "../assets/fonts/AvenirNextLTPro-Bold.ttf",
+  declarations: [{ prop: "ascent-override", value: "100%" }],
 });
 export const demiFont = localFont({
   src: "../assets/fonts/AvenirNextLTPro-Demi.ttf",
+  declarations: [{ prop: "ascent-override", value: "100%" }],
 });
 export const heavyFont = localFont({
   src: "../assets/fonts/AvenirNextLTPro-Heavy.ttf",
+  declarations: [{ prop: "ascent-override", value: "100%" }],
 });
 export const lightFont = localFont({
   src: "../assets/fonts/AvenirNextLTPro-Light.ttf",
+  declarations: [{ prop: "ascent-override", value: "100%" }],
 });
 export const mediumFont = localFont({
   src: "../assets/fonts/AvenirNextLTPro-Medium.ttf",
+  declarations: [{ prop: "ascent-override", value: "100%" }],
 });
 export const regularFont = localFont({
   src: "../assets/fonts/AvenirNextLTPro-Regular.ttf",
+  declarations: [{ prop: "ascent-override", value: "100%" }],
 });
