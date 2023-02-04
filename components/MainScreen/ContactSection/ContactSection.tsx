41595cfc31e8f69a56d69cb80bb658c66a8dd2bd
Lautaro Aguirre <lauta.nahuel94@gmail.com>
2023-02-04 00:22:26 -0300

    update contact section

--- components/MainScreen/ContactSection/ContactSection.tsx
+++ components/MainScreen/ContactSection/ContactSection.tsx
@@ -8,16 +8,16 @@ import * as S from "./Styled";
 
 export const ContactSection = () => {
   return (
-    <SectionContainer id="languages" mb="32px">
-      <SectionHeader title="Send me a message" />
+    <SectionContainer id="languages" mb="86px" mt="86px">
+      <SectionHeader title="Send me a message!" />
 
-      <Container>
+      <Container display="flex" justifyContent="center" mt="32px">
         <S.ExternalLink
           target="_blank"
           rel="noopener noreferrer"
           href="https://www.linkedin.com/in/lautaguirre"
         >
-          <LinkedInIcon size={86} />
+          <LinkedInIcon size={128} />
         </S.ExternalLink>
       </Container>
     </SectionContainer>
