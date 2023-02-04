100735a09c102237fd9707d45161234de0b75e42
Lautaro Aguirre <lauta.nahuel94@gmail.com>
2023-02-04 00:27:25 -0300

    add section margin

--- components/MainScreen/LanguagesSection/LanguagesSection.tsx
+++ components/MainScreen/LanguagesSection/LanguagesSection.tsx
@@ -42,7 +42,7 @@ export const LanguagesSection = () => {
   };
 
   return (
-    <SectionContainer id="languages">
+    <SectionContainer id="languages" mb="64px">
       <SectionHeader title="Languages" />
 
       <Container>{sortedLanguages.map(renderLanguages)}</Container>
